eb46338cea99863e84fc73c049100570447e81fb
Henrik Gustafsson <henrik.gustafsson@proceranetworks.com>
2014-10-10 13:57:10 +0200

    Make data asserts byte order aware

--- src/main/java/se/fnord/katydid/DataAsserts.java
+++ src/main/java/se/fnord/katydid/DataAsserts.java
@@ -3,6 +3,7 @@ package se.fnord.katydid;
 import se.fnord.katydid.internal.TestingContextImpl;
 
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class DataAsserts {
 	private static void assertNext(TestingContext tc, DataTester dataTester, ByteBuffer bb) {
@@ -40,12 +41,30 @@ public class DataAsserts {
 	 * @param bb the ByteBuffer to test
 	 */
 	public static void assertExact(DataTester tester, ByteBuffer bb) {
-		ByteBuffer bb2 = bb.slice();
+		ByteBuffer bb2 = bb.slice().order(bb.order());
 		TestingContextImpl tc = new TestingContextImpl(bb2);
 		if (bb2.remaining() != tester.length())
 			tc.addFailure("%d bytes data expected, was %d bytes", tester.length(), bb.remaining());
 		assertNext(tc, tester, bb2);
 		tc.assertSuccess(tester);
+	}
+
+	/**
+	 * Asserts that {@code bytes} exactly matches the tester.
+	 * {@code bytes} is treated as being big endian.
+	 * <p>
+	 * Equivalent to calling
+	 * <p>
+	 * <code>
+	 *     assertExact(tester, ByteBuffer.wrap(bytes).order(ByteOrder.BIG_ENDIAN))
+	 * </code>
+	 *
+	 * @throws java.lang.AssertionError if a value does not match or {@code bb} does not contain exactly {@code tester.length()} bytes.
+	 * @param tester the tester to use when verifying the buffer.
+	 * @param bytes the bytes to test
+	 */
+	public static void assertExact(DataTester tester, byte[] bytes) {
+		assertExact(tester, ByteOrder.BIG_ENDIAN, bytes);
 	}
 
 	/**
@@ -62,8 +81,8 @@ public class DataAsserts {
 	 * @param tester the tester to use when verifying the buffer.
 	 * @param bytes the bytes to test
 	 */
-	public static void assertExact(DataTester tester, byte[] bytes) {
-		assertExact(tester, ByteBuffer.wrap(bytes));
+	public static void assertExact(DataTester tester, ByteOrder order, byte[] bytes) {
+		assertExact(tester, ByteBuffer.wrap(bytes).order(order));
 	}
 
 	/**
@@ -75,6 +94,21 @@ public class DataAsserts {
 	public static ByteBuffer asBuffer(DataTester tester) {
 		int length = tester.length();
 		ByteBuffer bb = ByteBuffer.allocate(length);
+		tester.toBuffer(bb);
+		bb.flip();
+		return bb;
+	}
+
+	/**
+	 * Creates a ByteBuffer with contents that will match {@code tester} when used as input to {@code assertExact()}
+	 *
+	 * @param order the byte order to use when generating the buffer.
+	 * @param tester the tester to use when generating the buffer.
+	 * @return the ByteBuffer containing the generated data.
+	 */
+	public static ByteBuffer asBuffer(ByteOrder order, DataTester tester) {
+		int length = tester.length();
+		ByteBuffer bb = ByteBuffer.allocate(length).order(order);
 		tester.toBuffer(bb);
 		bb.flip();
 		return bb;
--- src/test/java/se/fnord/katydid/TestDataAsserts.java
+++ src/test/java/se/fnord/katydid/TestDataAsserts.java
@@ -2,7 +2,11 @@ package se.fnord.katydid;
 
 import org.junit.Test;
 
-import static se.fnord.katydid.DataTesters.h8;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import static org.junit.Assert.assertEquals;
+import static se.fnord.katydid.DataTesters.*;
 import static se.fnord.katydid.DataTesters.struct;
 import static se.fnord.katydid.internal.Util.bytes;
 
@@ -24,6 +28,38 @@ public class TestDataAsserts {
 			struct("struct",
 				h8("ap", 1, 2, 3, 4)),
 			bytes(1, 2, 3, 4));
+	}
+
+	@Test
+	public void testAssertExactByteOrderWithBuffer() {
+		ByteBuffer bigEndian = ByteBuffer.allocate(4);
+		bigEndian.order(ByteOrder.BIG_ENDIAN);
+		bigEndian.putInt(0x12345678);
+		bigEndian.flip();
+		DataAsserts.assertExact(h32(0x12345678), bigEndian);
+
+		ByteBuffer littleEndian = ByteBuffer.allocate(4);
+		littleEndian.order(ByteOrder.LITTLE_ENDIAN);
+		littleEndian.putInt(0x12345678);
+		littleEndian.flip();
+		DataAsserts.assertExact(h32(0x12345678), littleEndian);
+	}
+
+	@Test
+	public void testAssertExactByteOrderWithByteArray() {
+		DataAsserts.assertExact(h32(0x12345678), ByteOrder.BIG_ENDIAN, new byte[] { 0x12, 0x34, 0x56, 0x78 });
+		DataAsserts.assertExact(h32(0x12345678), ByteOrder.LITTLE_ENDIAN, new byte[] { 0x78, 0x56, 0x34, 0x12 });
+	}
+
+	@Test
+	public void testAsBuffer() {
+		ByteBuffer bigEndian = ByteBuffer.wrap(new byte[] { 0x12, 0x34, 0x56, 0x78 });
+		bigEndian.order(ByteOrder.BIG_ENDIAN);
+		assertEquals(bigEndian, DataAsserts.asBuffer(ByteOrder.BIG_ENDIAN, h32(0x12345678)));
+
+		ByteBuffer littleEndian = ByteBuffer.wrap(new byte[] { 0x78, 0x56, 0x34, 0x12 });
+		littleEndian.order(ByteOrder.LITTLE_ENDIAN);
+		assertEquals(littleEndian, DataAsserts.asBuffer(ByteOrder.LITTLE_ENDIAN, h32(0x12345678)));
 	}
 
 	@Test(expected = AssertionError.class)
